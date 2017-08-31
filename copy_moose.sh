3fff978a6b09b99487be01f5d3bcc3599b2efb8b
Alex Lindsay <alexlindsay239@gmail.com>
2017-08-31 14:43:15 -0600

    Modify copy_moose for working on mac (e.g. bash3)

--- copy_moose.sh
+++ copy_moose.sh
@@ -7,9 +7,11 @@ path=$PWD
 new_path="${path/src/include}"
 
 cp $OLD".C" $NEW".C"
-sed -i "s/$OLD/$NEW/g" $NEW".C"
+sed -e "s/$OLD/$NEW/g" -i ".bak" $NEW".C"
 
 cd $new_path
 cp $OLD".h" $NEW".h"
-sed -i "s/$OLD/$NEW/g" $NEW".h"
-sed -i "s/${OLD^^}/${NEW^^}/g" $NEW".h"
+old_upper=$(echo "$OLD" | tr '[:lower:]' '[:upper:]')
+new_upper=$(echo "$NEW" | tr '[:lower:]' '[:upper:]')
+sed -e "s/$OLD/$NEW/g" -i ".bak" $NEW".h"
+sed -e "s/$old_upper/$new_upper}/g" -i ".bak" $NEW".h"
